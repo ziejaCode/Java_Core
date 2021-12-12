bcb711d0acd736756ee2c813277d5a8bc2e62a8d
ziejaCode <49070294+ziejaCode@users.noreply.github.com>
2021-12-12 15:18:41 +0000

    Static, non static

---
+++ JavaCore/src/_1_variables/_1_7_Static_and_nonStatic_Variables/BlockOfStatics.java
@@ -0,0 +1,23 @@
+package _1_variables._1_7_Static_and_nonStatic_Variables;
+/** @author Marcin
+ *  This application is about 
+ **/
+public class BlockOfStatics {
+    
+	    private static String name;
+		private static String marian;
+
+		static{
+	        int marian = 23;
+	        String name;
+	    }
+	      
+	   static String print(){
+	        System.out.print(name + " " + marian);
+	        return name;
+	   }
+	   
+	   public static void main(String []args){
+	       String s = name + print();
+	   }
+}
---
+++ JavaCore/src/_1_variables/_1_7_Static_and_nonStatic_Variables/StaticVaribleI.java
@@ -0,0 +1,14 @@
+package _1_variables._1_7_Static_and_nonStatic_Variables;
+
+// created by Marcin 
+public class StaticVaribleI {
+    
+        public static void main(String args[]){
+
+
+
+
+        }
+
+}
+
