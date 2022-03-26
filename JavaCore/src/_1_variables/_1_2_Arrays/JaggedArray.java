a2d0afcb8d252ccb851bde26ab4d565e11ead12f
ziejaCode <49070294+ziejaCode@users.noreply.github.com>
2022-03-26 13:04:04 +0000

    Arrays update 2

--- JavaCore/src/_1_variables/_1_2_Arrays/JaggedArray.java
+++ JavaCore/src/_1_variables/_1_2_Arrays/JaggedArray.java
@@ -1,6 +1,13 @@
 package _1_variables._1_2_Arrays;
 
 // created by Marcin 
+
+/*
+ * In computer science, a jagged array, also known as a ragged array, 
+ * is an array of arrays of which the member arrays can be of different 
+ * lengths, producing rows of jagged edges when visualized as output
+ */
+
 public class JaggedArray {
         public static void main(String args[]){
 
--- JavaCore/src/_1_variables/_1_2_Arrays/MultiDimmentsionalArray.java
+++ JavaCore/src/_1_variables/_1_2_Arrays/MultiDimmentsionalArray.java
@@ -24,7 +24,7 @@ public class MultiDimmentsionalArray {
             
                 for(int i = 0; i  < nums.length;i++){
                     for(int j = 0; j < nums[i].length;j++){
-                        System.out.print(nums[i][j] + " ");
+                        System.out.print(i + j + " ");
                     }System.out.println();
                 }
             System.out.println(Arrays.deepToString(nums)); // 
